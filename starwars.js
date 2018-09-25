1f69fbb0502bc2c702ce1d6d4430db9cef27637f
bpoliana <bpolianajaber@gmail.com>
2018-09-25 12:04:13 -0300

    Add jquery and star wars theme song

--- starwars.js
+++ starwars.js
@@ -3,3 +3,33 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+$(document).ready(() => {
+  let episode = localStorage.getItem('episode');
+  if(episode)
+    setIntro(episode);
+});
+
+const id2episode = id => {
+  let episodes = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII'];
+  return episodes[parseInt(id)];
+}
+
+const setIntro = text => {
+  localStorage.setItem('episode', text);
+  $('.reading-animation').html(text);
+}
+
+$.ajax({
+  url: 'url-da-requisicao',
+  dataType: 'json',
+  success: function(resposta) {
+    // escreve a resposta no console (para ver o que foi recebido)
+    console.log(resposta);
+    // faz o que quiser fazer com a resposta...
+    // ...
+  }
+});
+
+
+$('#movies').
